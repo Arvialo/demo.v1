fb22c332bd9c9ab6645ce771c866fb5adea30b71
AnithK7 <kandasamy@et.esiea.fr>
2023-06-03 01:34:25 +0200

    update

--- components/Footer.js
+++ components/Footer.js
@@ -10,16 +10,35 @@ function Footer() {
           <p>07 82 52 63 67</p>
         </div>
         <div className="footer-plan">
-          <h2 data-aos="fade-right" data-aos-once="true">
+          <h2
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-anchor-placement="center-bottom"
+          >
             Accueil
           </h2>
-          <h2 data-aos="fade-right" data-aos-once="true" data-aos-delay="50">
+          <h2
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="50"
+            data-aos-anchor-placement="center-bottom"
+          >
             Services
           </h2>
-          <h2 data-aos="fade-right" data-aos-once="true" data-aos-delay="100">
+          <h2
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="100"
+            data-aos-anchor-placement="center-bottom"
+          >
             Réalisations
           </h2>
-          <h2 data-aos="fade-right" data-aos-once="true" data-aos-delay="150">
+          <h2
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="150"
+            data-aos-anchor-placement="center-bottom"
+          >
             Contactez-nous
           </h2>
         </div>
